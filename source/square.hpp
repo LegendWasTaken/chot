8ff266e35253ddb224a88cea9ab0c676c27d6504
LegendWasTaken <caiodcw@gmail.com>
2023-07-05 16:03:01 +0800

    i know better than the compiler

--- source/square.hpp
+++ source/square.hpp
@@ -36,26 +36,48 @@ namespace chot {
 
         std::uint8_t data = 0; // & 0x3 = file, & 0x38 = rank
 
-        [[nodiscard]] static std::uint8_t data_for_index(chot::square_index index) noexcept;
+        [[nodiscard]] static std::uint8_t data_for_index(chot::square_index index) noexcept {
+            const auto file = index % 8;
+            const auto rank = index / 8;
+            return (file << file_shift) | (rank << rank_shift);
+        }
 
     public:
-        square(std::uint8_t file, std::uint8_t rank);
+        __attribute__((always_inline)) square(std::uint8_t file, std::uint8_t rank) :
+                data((file << file_shift) | (rank << rank_shift)) {
+        }
 
-        square(chot::file file, chot::rank rank);
+        __attribute__((always_inline)) square(chot::file file, chot::rank rank) :
+                data((static_cast<std::uint8_t>(file) << file_shift) | static_cast<std::uint8_t>(rank) << rank_shift) {
+        };
 
-        explicit square(chot::square_index index);
+        __attribute__((always_inline)) explicit square(chot::square_index index): data(data_for_index(index)) {
+        }
 
-        [[nodiscard]] static square from_bits(std::uint64_t bits);
+        [[nodiscard]] chot::square_index index() const noexcept {
+            const auto file_num = (data >> file_shift) & 0x7;
+            const auto rank_num = (data >> rank_shift) & 0x7;
+            return file_num + rank_num * 8;
+        }
 
-        [[nodiscard]] chot::square_index index() const noexcept;
+        [[nodiscard]] chot::file file() const noexcept {
+            const auto file_num = (data >> file_shift) & 0x7;
+            return static_cast<chot::file>(file_num);
+        }
 
-        [[nodiscard]] chot::file file() const noexcept;
 
-        [[nodiscard]] chot::rank rank() const noexcept;
+        [[nodiscard]] chot::rank rank() const noexcept {
+            const auto rank_num = (data >> rank_shift) & 0x7;
+            return static_cast<chot::rank>(rank_num);
+        }
 
-        [[nodiscard]] bool operator==(const chot::square &rhs) const noexcept;
+        [[nodiscard]] bool operator==(const chot::square &rhs) const noexcept {
+            return data == rhs.data;
+        }
 
-        [[nodiscard]] bool operator!=(const chot::square &rhs) const noexcept;
+        [[nodiscard]] bool operator!=(const chot::square &rhs) const noexcept {
+            return !this->operator==(rhs);
+        }
     };
 
     inline std::ostream &operator<<(std::ostream &stream, const chot::square &square) {
